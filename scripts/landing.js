81b93870efe916b909b4e5feeea99714175aa678
megansawkar <megan.sawkar@gmail.com>
2017-02-07 09:31:21 -0800

    Update from Assignment 22

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,16 @@
 var animatePoints = function() {
     var points = document.getElementsByClassName('point');
     
-    var revealPoint = function(points) {
-        for (var i = 0; i < points.length; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
+    var revealPoint = function(index) {
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scaleX(1) translateY(0)";
+            points[index].style.msTransform = "scaleX(1) translateY(0)";
+            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
     
-    revealPoint();
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
 };
 
 animatePoints();
