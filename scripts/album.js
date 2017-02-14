990cc6c57ed990bd4bcc948dc61f426de9134614
megansawkar <megan.sawkar@gmail.com>
2017-02-13 17:38:58 -0800

    Checkpoint 27-PlayPause2: Complete

--- scripts/album.js
+++ scripts/album.js
@@ -71,10 +71,33 @@ var findParentByClassName = function(element, theClass) {
 
 var getSongItem = function(element) {
 		switch (element.className) {
-				case :
-					return "";
-				default: 
-					return;
+			case 'song-item-number':
+				return element;
+			case 'song-item-title':
+			case 'song-item-duration':
+				return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+			case 'ion-play':
+			case 'album-song-button':
+				return findParentByClassName(element, 'song-item-number');
+			default: 
+				return;
+		}
+};
+
+var clickHandler = function(targetElement) {
+		var songItem = getSongItem(targetElement);
+	
+		if (currentlyPlayingSong === null) {
+			songItem.innerHTML = pauseButtonTemplate;
+			currentlyPlayingSong = songItem.getAttribute('data-song-number');
+		} else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+			songItem.innerHTML = playButtonTemplate;
+			currentlyPlayingSong = null;
+		} else if (currentlyPlayingSong !== songItem.getAttribute ('data-song-number')) {
+			var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+			currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+			songItem.innerHTML = pauseButtonTemplate;
+			currentlyPlayingSong = songItem.getAttribute('data-song-number');
 		}
 };
 
@@ -82,6 +105,9 @@ var songListContainer = document.getElementsByClassName('album-view-song-list')[
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a  class="album-song-button"><span class="ion-pause"></span></a>';
+
+var currentlyPlayingSong = null;
  
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
@@ -89,12 +115,26 @@ window.onload = function() {
 	songListContainer.addEventListener('mouseover', function(event) {
 		if (event.target.parentElement.className === 'album-view-song-item') {
         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-    }
+		
+			var songItem = getSongItem(event.target);
+
+			if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+				songItem.innerHTML = playButtonTemplate;
+			}
+	}
 	});
 	
 	for (var i = 0; i < songRows.length; i++) {
 		songRows[i].addEventListener('mouseleave', function(event) {
-			this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+			var songItem = getSongItem(event.target);
+			var songItemNumber = songItem.getAttribute('data-song-number');
+			
+			if (songItemNumber !== currentlyPlayingSong) {
+				songItem.innerHTML = songItemNumber;
+			}
+		});
+		songRows[i].addEventListener('click', function(event) {
+			clickHandler(event.target);
 		});
 	}
 };
